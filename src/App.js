ca5579ca387fe150394bdaca9655cad16e7cfd78
Sajeesh <sajeeshsaju003@gmail.com>
2023-10-15 15:26:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         
           <Routes>
       
-            <Route path="/" element={<Home />}/>
+            <Route path="https://Sajeeshp94.github.io/react-digital-portfolio" element={<Home />}/>
             <Route path="/about" element={<About/>}/>
             <Route path="/projects" element={<Projects/>}/>
               
